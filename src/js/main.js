2c38560c3055a09c73186831868b8e3ba5139c7e
silverwolf87 <andrescaceres776@gmail.com>
2024-05-23 13:27:37 -0500

    fix:barra de navegacion

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,19 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+const iconOpen = document.querySelector('.icon-open');
+const iconClose = document.querySelector('.icon-close')
+const navMobile = document.querySelector('.nav-mobile');
+
+
+
+iconClose.addEventListener('click', ()=>{
+    navMobile.classList.toggle('hiden');
+    iconOpen.classList.remove('btn-hidden');
+})
+
+iconOpen.addEventListener('click', ()=>{
+    navMobile.classList.toggle('hiden');
+    iconOpen.classList.add('btn-hidden');
+})
