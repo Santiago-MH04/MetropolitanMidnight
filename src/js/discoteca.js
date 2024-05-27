750e5d4954d4a8c96bdb30b9aac018103b332a73
Deilons <apolo4748@gmail.com>
2024-05-27 07:49:08 -0500

    fix: arreglo de las paginas con el id

--- src/js/discoteca.js
+++ src/js/discoteca.js
@@ -1,14 +1,83 @@
-//codigo para el slider
 
-const $next = document.querySelector(".next");
-const $prev = document.querySelector(".prev");
+document.addEventListener("DOMContentLoaded", ()=>{
+   
+   fetch('../../public/data/discotecasDB.json')
+   .then(response => response.json())
+   .then(data => mostrarDisco(data))
+   .catch(error => console.error('Error al cargar el archivo JSON:', error));
+})
 
-$next.addEventListener("click", () => {
-    const items = document.querySelectorAll(".item");
-    document.querySelector(".slide").appendChild(items[0]);
-});
+function mostrarDisco(data){
+    const idDisco = JSON.parse(localStorage.getItem("id_discoteca"))
+    const discoteca = data.filter(discoteca  =>discoteca.id == idDisco)
+    console.log(discoteca);
 
-$prev.addEventListener("click", () => {
-    const items = document.querySelectorAll(".item");
-    document.querySelector(".slide").prepend(items[items.length - 1]);
-});
+    const contenedor = document.querySelector(".container_main")
+ contenedor.innerHTML=`
+ <section class="slider">
+ <div class="container_slider">
+     <div class="slide">
+         <div class="item"
+             style="background-image: url(${discoteca[0].banner}) ;background-size: cover;">
+             <div class="content">
+                 <div class="name">${discoteca[0].logo}</div>
+            </div>
+         </div>
+     </div>
+
+         
+</section>
+
+<section class="info">
+ <div class="services">
+    <h1> ${discoteca[0].name}</h1>
+     <div class="service_logo">
+         <i class="fa-solid fa-book"></i>
+     </div>
+     <div class="service_title"> informacion</div>
+     <div class="service_description">
+         <p>${discoteca[0].description} </p>
+
+         <p id="hours_title">Horarios</p>
+         <p id="hours_description">${discoteca[0].hours}</p>
+
+         <p id="price_title">precios</p>
+         <p id="price_description">${discoteca[0].price.entry}<hr>${discoteca[0].price.drinks}</p>
+     </div>
+ </div>
+
+ <div class="ubication">
+     <div class="ubication_logo">
+         <i class="fa-solid fa-location-dot"></i>
+     </div>
+     <div class="ubication_title">ubicacion</div>
+     <div class="ubication_description">
+     ${discoteca[0].location.map}
+     </div>
+</section>
+<section class="reviews">
+ <div class="review">
+     <div class="review_img">
+         <img src="../../public/img/discotecas/MIRANDA/miranda1.webp" alt="">
+     </div>
+     <div class="review_name">
+         <p>Carlos</p>
+     </div>
+     <div class="review_description">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, ipsam.</p>
+     </div>
+     <div class="review_stars">
+         <i class="fa-solid fa-star"></i>
+         <i class="fa-solid fa-star"></i>
+         <i class="fa-solid fa-star"></i>
+         <i class="fa-solid fa-star"></i>
+         <i class="fa-solid fa-star"></i>
+     </div>
+ </div>
+ 
+
+</section>
+
+`
+
+}
--- src/js/discotecas.js
+++ src/js/discotecas.js
@@ -17,36 +17,48 @@ $prev.addEventListener("click", () => {
 
 //imprimir en el html la info de las discotecas de la base de datos
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const menuDiscotecas = document.getElementById('menu_discotecas');
     const container = document.querySelector('.discotecas');
 
-    
+
     fetch('../../public/data/discotecasDB.json')
         .then(response => response.json())
         .then(data => renderDiscotecas(data))
         .catch(error => console.error('Error al cargar el archivo JSON:', error));
 
     function renderDiscotecas(discotecas) {
-        
+
         container.innerHTML = '';
         const zonaSeleccionada = menuDiscotecas.value;
         if (zonaSeleccionada === 'Selecciona tu zona') {
-            discotecas.forEach(disco => renderizarDisco(disco));
+            discotecas.forEach(disco => {
+                renderizarDisco(disco)
+
+                const cardsLinks = document.querySelectorAll(".card-link")
+
+                cardsLinks.forEach(linkDisco => {
+                    linkDisco.addEventListener("click", (e) => {
+                        const id_discoteca = e.target.getAttribute("id_discoteca")
+                        localStorage.setItem("id_discoteca", JSON.stringify(id_discoteca))
+                        window.location = "discoteca.html"
+                    })
+
+                } );
+            })
         } else {
-            
+
             const discotecasFiltradas = filtrarDiscotecasPorZona(discotecas, zonaSeleccionada);
-            
+
             discotecasFiltradas.forEach(disco => renderizarDisco(disco));
         }
     }
-
 
     function renderizarDisco(disco) {
         const discoElement = document.createElement('div');
         discoElement.classList.add('disco');
         discoElement.innerHTML = `
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 18rem;" >
         <img src="${disco.logo}" class="card-img-top" alt="logo de ${disco.name}">
         <div class="card-body">
             <h5 class="card-title">${disco.name}</h5>
@@ -64,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function() {
             </li>
         </ul>
         <div class="card-body">
-            <a href="discoteca.html" class="card-link">ver mas</a>
+            <a href="#" class="card-link"  id_discoteca=${disco.id}>ver mas</a>
         </div>
         </div>
         `;
@@ -73,22 +85,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Función para filtrar las discotecas por zona
     function filtrarDiscotecasPorZona(discotecas, zona) {
-        return discotecas.filter(disco => disco.location.zone === zona );
+        return discotecas.filter(disco => disco.location.zone === zona);
     }
 
-    
-    menuDiscotecas.addEventListener('change', function() {
-        
+
+    menuDiscotecas.addEventListener('change', function () {
+
         fetch('../../public/data/discotecasDB.json')
             .then(response => response.json())
             .then(data => renderDiscotecas(data))
             .catch(error => console.error('Error al cargar el archivo JSON:', error));
-    });
-});
 
-//estrellas
 
-document.addEventListener("DOMContentLoaded", () => {
+
+    });   //estrellas
+
     const stars = document.querySelectorAll(".star");
     stars.forEach((star) => {
         star.addEventListener("click", () => {
@@ -101,5 +112,5 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     });
-    
 });
+
