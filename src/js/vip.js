0460508579e08c4a882fa77c09a6ecbb8592fe41
silverwolf87 <andrescaceres776@gmail.com>
2024-05-28 20:16:52 -0500

    fix:organizando responsive

--- src/js/vip.js
+++ src/js/vip.js
@@ -8,7 +8,6 @@ import * as bootstrap from 'bootstrap'
 // -------------------------------ACA COMIENZA LA LOGICA DE VIP---------------------------
 
 const main = document.querySelector("main")
-    main.innerHTML = "";
 const datos = await fetch(`http://localhost:3000/planes`)
 const planes = await datos.json()
 
@@ -28,7 +27,7 @@ planes.forEach(plan => {
                                     <li class="card-text">${plan.descripcion[0]}</li>
                                     <li class="card-text">${plan.descripcion[1]}</li>
                                     <li class="card-text">${plan.descripcion[2]}</li>
-                                <a href="" class="btn btn-primary">Obtener</a>
+                                <a href="https://www.paypal.com/co/home" class="btn btn-primary">Obtener</a>
                             </div>
                         </div>
                    
@@ -38,28 +37,4 @@ planes.forEach(plan => {
 });
 
 
-/*for (let i = 0; i < planes.length; i++) {
-   main.innerHTML +=`
-   <article id="vip-card">
-            <div class="background"></div>
-            <div class="grid">
-                <div>
-                    <article class="card-vip">
-                        <div class="container-card">
-                            <div class="card-header">
-                                <h2>${planes[i].nombre}</h2>
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title">${planes[i].costo}/mes</h5>
-                                <ul>
-                                    <li class="card-text">${planes[i].descripcion[0]}
-                                        </li>
-                                    <li class="card-text">${planes[i].descripcion[1]}</li>
-                                    <li class="card-text">${planes[i].descripcion[2]}</li>
-                                <a href="" class="btn btn-primary">Obtener</a>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-   `  
-}*/
+
